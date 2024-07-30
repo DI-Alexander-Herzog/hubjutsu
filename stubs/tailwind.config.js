33da91e2f98448a60338f18fff42fa2f75d5c165
Alexander Herzog <alexander@alexander-herzog.at>
2024-07-30 15:22:54 +0200

    Squashed 'packages/aherzog/starconnect/' content from commit bbb6c45
    git-subtree-dir: packages/aherzog/starconnect
    git-subtree-split: bbb6c45db46a63e0c626aea8af836fa0635989c7

---
+++ stubs/tailwind.config.js
@@ -0,0 +1,25 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.tsx',
+        './vendor/aherzog/starconnect-ui/resources/js',
+        './node_modules/primereact/**/*.{js,ts,jsx,tsx}',
+    ],
+
+    darkMode: 'selector',
+    
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [require('@tailwindcss/forms')],
+};
