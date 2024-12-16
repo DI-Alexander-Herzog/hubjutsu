a7f7abc0576f1aacfbe91e5ec91a524c89cccf17
ONGEMA tools <tools@ongema.com>
2024-12-16 13:22:41 +0100

    Füge Authentifizierung für LogViewer hinzu, um den Zugriff basierend auf der E-Mail-Domain des Benutzers zu steuern

--- src/HubjutsuServiceProvider.php
+++ src/HubjutsuServiceProvider.php
@@ -6,8 +6,10 @@ use AHerzog\Hubjutsu\App\Menu\MenuManager;
 use AHerzog\Hubjutsu\Console\HubjutsuGitCommand;
 use AHerzog\Hubjutsu\Console\HubjutsuSetupCommand;
 use AHerzog\Hubjutsu\Events\BuildMenuEvent;
+use Auth;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\ServiceProvider;
+use LogViewer;
 
 class HubjutsuServiceProvider extends ServiceProvider
 {
@@ -36,6 +38,16 @@ class HubjutsuServiceProvider extends ServiceProvider
             $this->unsignedBigInteger('updated_by')->nullable();
             $this->foreign('created_by')->references('id')->on('users')->nullOnDelete()->cascadeOnUpdate();
             $this->foreign('updated_by')->references('id')->on('users')->nullOnDelete()->cascadeOnUpdate();
+        });
+
+        LogViewer::auth(function ($request) {
+            if (!Auth::check()) return false;
+            
+            $domain = preg_replace('/^.*@/', '', Auth::getUser()->email);
+            $rootDomain = preg_replace('/^.*\.([^\.]+\.[^\.]+)/', '\1', parse_url(config('app.url'), PHP_URL_HOST) );
+
+            return in_array($domain, ['alexander-herzog.at', 'ongema.com', $rootDomain] );
+            
         });
     }
     
