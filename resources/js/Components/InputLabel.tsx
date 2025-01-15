c671094ed4e36ea791d191d4aa24ab701224f00d
Alexander Herzog <alexander@alexander-herzog.at>
2025-01-15 13:51:51 +0100

    Füge Unterstützung für dunkle Modi im InputLabel hinzu: aktualisiere die Klassen für die Textfarbe

--- resources/js/Components/InputLabel.tsx
+++ resources/js/Components/InputLabel.tsx
@@ -2,7 +2,7 @@ import { LabelHTMLAttributes } from 'react';
 
 export default function InputLabel({ value, className = '', children, ...props }: LabelHTMLAttributes<HTMLLabelElement> & { value?: string }) {
     return (
-        <label {...props} className={`block font-medium text-sm ` + className}>
+        <label {...props} className={`dark:text-gray-100 block font-medium text-sm ` + className}>
             {value ? value : children}
         </label>
     );
