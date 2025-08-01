5c04f380cc9dd0920cb273278a7932637cf56f5c
Alexander Herzog <alexander@alexander-herzog.at>
2025-08-01 14:51:01 +0200

    Verbessere die Textfarbe des Checkbox-Elements für bessere Sichtbarkeit

--- resources/js/Components/Checkbox.tsx
+++ resources/js/Components/Checkbox.tsx
@@ -7,7 +7,7 @@ export default function Checkbox({ className = '', ...props }: InputHTMLAttribut
                 {...props}
                 type="checkbox"
                 className={
-                    'rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-primary-600 shadow-sm focus:ring-primary-500 dark:focus:ring-primary-600 dark:focus:ring-offset-gray-800 ' +
+                    'rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-primary-500 shadow-sm focus:ring-primary-500 dark:focus:ring-primary-500 dark:focus:ring-offset-gray-800 ' +
                     className
                 }
             />
