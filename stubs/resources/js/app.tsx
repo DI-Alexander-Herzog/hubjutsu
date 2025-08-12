5082f6781ba5c3051149568cde712f56c17956f9
Alexander Herzog <alexander@alexander-herzog.at>
2025-08-12 09:33:51 +0200

    feat: füge ErrorBoundary um die Hauptanwendung hinzu und integriere Farbvariablen aus den shared props

--- stubs/resources/js/app.tsx
+++ stubs/resources/js/app.tsx
@@ -5,6 +5,8 @@ import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { LaravelReactI18nProvider } from 'laravel-react-i18n';
+import ErrorBoundary from '@hubjutsu/Components/ErrorBoundary';
+
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -14,16 +16,25 @@ createInertiaApp({
     setup({ el, App, props }) {
         const root = createRoot(el);
 
+        // Farben aus den shared props holen
+            if ((props.initialPage.props?.hub as any).cssVars) {
+                const styleTag = document.createElement('style');
+                styleTag.id = 'hub-colors';
+                styleTag.innerHTML = (props.initialPage.props.hub as any)?.cssVars as string;
+                document.head.appendChild(styleTag);
+            }
             
         root.render(
-            <LaravelReactI18nProvider
-                locale={'de'}
-                fallbackLocale={'en'}
-                files={import.meta.glob('/lang/*.json')}
-            >
-            
-                <App {...props} />
-            </LaravelReactI18nProvider>
+            <ErrorBoundary>
+                <LaravelReactI18nProvider
+                    locale={'de'}
+                    fallbackLocale={'en'}
+                    files={import.meta.glob('/lang/*.json')}
+                >
+                
+                    <App {...props} />
+                </LaravelReactI18nProvider>
+            </ErrorBoundary>
         );
     },
     progress: {
