1dc7060976d9dea6804fc7aeef3b3c953fd67d8b
Abhay <abhay.rathod@nexuslinkservices.in>
2025-08-13 15:46:42 +0530

    feat: add reusable form component with title, subtitle, and input fields elements

--- resources/js/Pages/Profile/Edit.tsx
+++ resources/js/Pages/Profile/Edit.tsx
@@ -1,39 +1,38 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import DeleteUserForm from '@/Pages/Profile/Partials/DeleteUserForm';
-import UpdatePasswordForm from '@/Pages/Profile/Partials/UpdatePasswordForm';
-import UpdateProfileInformationForm from '@/Pages/Profile/Partials/UpdateProfileInformationForm';
-import { Head } from '@inertiajs/react';
-import { PageProps } from '@/types';
-import UpdateAvatarForm from './Partials/UpdateAvatarForm';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import DeleteUserForm from "@/Pages/Profile/Partials/DeleteUserForm";
+import UpdatePasswordForm from "@/Pages/Profile/Partials/UpdatePasswordForm";
+import UpdateProfileInformationForm from "@/Pages/Profile/Partials/UpdateProfileInformationForm";
+import { Head } from "@inertiajs/react";
+import { PageProps } from "@/types";
+import UpdateAvatarForm from "./Partials/UpdateAvatarForm";
 
-export default function Edit({ mustVerifyEmail, status }: PageProps<{ mustVerifyEmail: boolean, status?: string }>) {
-    return (
-        <AuthenticatedLayout
-            title="Profile"
-        >
+export default function Edit({
+	mustVerifyEmail,
+	status,
+}: PageProps<{ mustVerifyEmail: boolean; status?: string }>) {
+	return (
+		<AuthenticatedLayout title="Profile">
+			<div className="py-12">
+				<div className="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+					<div className="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+						<UpdateProfileInformationForm
+							mustVerifyEmail={mustVerifyEmail}
+							status={status}
+						/>
+					</div>
+					<div className="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+						<UpdateAvatarForm className="max-w-xl" />
+					</div>
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-                    <div className="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                        <UpdateProfileInformationForm
-                            mustVerifyEmail={mustVerifyEmail}
-                            status={status}
-                            className="max-w-xl"
-                        />
-                    </div>
-                    <div className="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                        <UpdateAvatarForm className="max-w-xl" />
-                    </div>
+					<div className="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+						<UpdatePasswordForm className="max-w-xl" />
+					</div>
 
-                    <div className="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                        <UpdatePasswordForm className="max-w-xl" />
-                    </div>
-                    
-                    <div className="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                        <DeleteUserForm className="max-w-xl" />
-                    </div>
-                </div>
-            </div>
-        </AuthenticatedLayout>
-    );
+					<div className="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+						<DeleteUserForm className="max-w-xl" />
+					</div>
+				</div>
+			</div>
+		</AuthenticatedLayout>
+	);
 }
