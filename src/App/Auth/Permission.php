e0dafcc94c9935dbcf7e3d53ef11fcca2f3c5349
Alexander Herzog <alexander@alexander-herzog.at>
2025-09-16 12:03:48 +0200

    feat: Implement getPermissionForUserAndHub method in Permission class and add PermissionPolicy class

--- src/App/Auth/Permission.php
+++ src/App/Auth/Permission.php
@@ -3,6 +3,10 @@
 
 namespace AHerzog\Hubjutsu\App\Auth;
 
+use AHerzog\Hubjutsu\Models\UserHubRole;
+use App\Models\Hub;
+use App\Models\User;
+
 class Permission {
     private static $groups = [];
     private static $permissions = [];
@@ -27,5 +31,17 @@ class Permission {
             $permissions = self::$permissions[$group] ?? [];
             yield $name => $permissions;
         }
+    }
+
+
+    static function getPermissionForUserAndHub(User $user, Hub $hub) {
+        $permissions = [];
+        UserHubRole::with('role.permissions')->where('user_id', $user->id)->where('hub_id', $hub->id)->get()->each(function($userHubRole) use (&$permissions) {
+            $userHubRole->role->permissions->each(function($permission) use (&$permissions) {
+                $permissions[$permission->name] = true;
+            });
+        });
+
+        return $permissions;
     }
 }
