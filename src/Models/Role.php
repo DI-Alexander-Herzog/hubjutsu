f0b022142ebd3d0bb7d0ae3e8b34d6240e199099
Alexander Herzog <alexander@alexander-herzog.at>
2025-09-16 10:42:01 +0200

    feat: Enhance Role model with UserTrait and define fillable attributes

--- src/Models/Role.php
+++ src/Models/Role.php
@@ -2,14 +2,17 @@
 
 namespace AHerzog\Hubjutsu\Models;
 
+use AHerzog\Hubjutsu\Models\Traits\UserTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use App\Models\Base;
 
 class Role extends Base
 {
-    use HasFactory; //HasTimestamp
+    use HasFactory, UserTrait; //HasTimestamp
 
-    protected $fillable = [];
+    protected $fillable = [
+        'name', 
+    ];
 
     protected $casts = [];
 
