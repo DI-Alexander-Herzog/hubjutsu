a27d3e388a3b48dc75a18786bf850de9807e3bc3
Abhay <abhay.rathod@nexuslinkservices.in>
2025-09-17 16:45:17 +0530

    fix: Add icons to breadcrumbs in Role and User pages for  navigation

--- resources/js/Pages/Role/Index.tsx
+++ resources/js/Pages/Role/Index.tsx
@@ -1,10 +1,15 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { CogIcon, ShieldCheckIcon } from "@heroicons/react/20/solid";
 import DataTable from "@hubjutsu/Components/DataTable";
 import DataTableLink from "@hubjutsu/Components/DataTableLink";
 
 export default function RoleIndex() {
 	return (
-		<AuthenticatedLayout title="Roles">
+		<AuthenticatedLayout title="Roles"
+			breadcrumbs={[
+				{ label: 'Settings', url: route('settings.index'), icon: <CogIcon /> },
+				{ label: 'Roles', icon: <ShieldCheckIcon /> }
+			]}>
 			<DataTable
 				routemodel="role"
 				columns={[
