c674c2a1be3dc51216566d2846e005340cc5610a
Alexander Herzog <alexander@alexander-herzog.at>
2025-09-22 13:19:52 +0200

    fix: Update ToggleSwitch background color to use primary color for checked state

--- resources/js/Components/InputToggle.tsx
+++ resources/js/Components/InputToggle.tsx
@@ -17,7 +17,7 @@ const ToggleSwitch = ({
         onClick={onChange}
         className={classNames(
             'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2',
-            checked ? 'bg-blue-600' : 'bg-gray-200 dark:bg-gray-700',
+            checked ? 'bg-primary-600' : 'bg-gray-200 dark:bg-gray-700',
             disabled && 'opacity-50 cursor-not-allowed'
         )}
         disabled={disabled}
