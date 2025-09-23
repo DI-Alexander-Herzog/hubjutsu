e564dff6d8ce948b0f7fe11ac75469bc7acd91f5
Alexander Herzog <alexander@alexander-herzog.at>
2025-09-23 14:12:50 +0200

    feat: Update boolean editor in DataTableFormatter to render as a styled checkbox

--- resources/js/Components/DataTableFormatter.tsx
+++ resources/js/Components/DataTableFormatter.tsx
@@ -22,7 +22,17 @@ const DataTableFormatter = {
 
     boolean: (row: Row, field: string) => {
         if (row[field] === undefined || row[field] === null) return "";
-        return row[field] ? "Ja" : "Nein";
+
+        const checked = row[field];
+        return <label className={"inline-flex items-center cursor-pointer align-middle"}>
+            <input type="checkbox" value="1" className="sr-only peer" checked={checked} />
+            <div className={
+                " relative w-9 h-5 bg-gray-200 " +
+                "rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white " + 
+                "after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full " +
+                "after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-primary-600 dark:peer-checked:bg-primary-600 "
+            }></div>
+        </label>;
     },
 
     color: (row: Row, field: string) => {
